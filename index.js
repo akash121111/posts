daf05b0b8f12b3d221fc9dccf5b3ddfcf8675d93
akash121111 <akashkumar121111@gmail.com>
2021-04-17 13:43:43 +0530

    localhost error

--- index.js
+++ index.js
@@ -9,9 +9,26 @@ const postRoutes = require('./routes/post');
 
 const app = express();
 
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', 'localhost:3000');
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With,auth, Content-Type,Access-Control-Allow-Origin,withcredentials, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+	next();
+});
+
+
 app.get('/', (rqe, res) => {
 	res.json({ primaryColor: '#aa3422', secondryColor: '#bbbbbb' });
 });
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', userRoutes);
@@ -24,6 +41,7 @@ mongoose.connect(db.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true,
 	if (err) console.log(err);
 	console.log('database is connected');
 });
+
 
 const PORT = 3000;
 app.listen(process.env.PORT || PORT, () => {
